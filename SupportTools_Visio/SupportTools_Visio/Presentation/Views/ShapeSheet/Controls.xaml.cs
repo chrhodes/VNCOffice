35993760c72cd533b8863a89730ad885f35abde6
Christopher Rhodes <chrhodes@cox.net>
2024-06-09 16:30:01 -0700

    Cleaned up XXX.xaml.cs

--- SupportTools_Visio/SupportTools_Visio/Presentation/Views/ShapeSheet/Controls.xaml.cs
+++ SupportTools_Visio/SupportTools_Visio/Presentation/Views/ShapeSheet/Controls.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using SupportTools_Visio.Presentation.ViewModels;
 using VNC;
 
@@ -10,9 +11,11 @@ namespace SupportTools_Visio.Presentation.Views
 
         public Controls()
         {
-            Log.Trace("Enter", Common.LOG_CATEGORY);
+            Int64 startTicks = Log.CONSTRUCTOR("Enter", Common.LOG_CATEGORY);
+
             InitializeComponent();
-            Log.Trace("Exit", Common.LOG_CATEGORY);
+
+            Log.CONSTRUCTOR("Exit", Common.LOG_CATEGORY, startTicks);
         }
 
         #endregion
--- SupportTools_Visio/SupportTools_Visio/Presentation/Views/ShapeSheet/ShapeLayout.xaml.cs
+++ SupportTools_Visio/SupportTools_Visio/Presentation/Views/ShapeSheet/ShapeLayout.xaml.cs
@@ -1,22 +1,21 @@
-﻿using System.Windows.Controls;
-using SupportTools_Visio.Presentation.ViewModels;
+﻿using System;
+using System.Windows.Controls;
+
 using VNC;
 
 namespace SupportTools_Visio.Presentation.Views
 {
     public partial class ShapeLayout : UserControl
     {
-        //private readonly ShapeLayoutViewModel _viewModel;
-
         #region Constructors and Load
 
         public ShapeLayout()
         {
-            Log.Trace("Enter", Common.LOG_CATEGORY);
+            Int64 startTicks = Log.CONSTRUCTOR("Enter", Common.LOG_CATEGORY);
+
             InitializeComponent();
-            //_viewModel = viewModel;
-            //DataContext = _viewModel;
-            Log.Trace("Exit", Common.LOG_CATEGORY);
+
+            Log.CONSTRUCTOR("Exit", Common.LOG_CATEGORY, startTicks);
         }
 
         #endregion
