23b0e06832b03bef09dd54e594665cb5f64f7659
Christopher Rhodes <chrhodes@cox.net>
2026-05-12 16:08:49 -0700

    Baseline for VNCDev2020 vs SR71

---
+++ VNCExcelTools/VNCExcelTools/Common.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace VNCExcelTools
+{
+    public class Common : VNCExcelToolsApplication.Common
+    {
+        new public const string LOG_CATEGORY = "VNCExcelTools";
+    }
+}
